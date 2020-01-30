c50049452d512555a5d2ed840d96c7ce5f4c3a52
Zahir Koradia <zahir@uplearn.co.uk>
2020-01-30 11:44:12 +0000

    prep for publishing to hex

--- mix.exs
+++ mix.exs
@@ -5,9 +5,23 @@ defmodule QueryElf.MixProject do
     [
       app: :query_elf,
       version: "0.1.0",
+      description: description(),
+      source_url: "https://gitlab.com/up-learn-uk/query-elf",
+      package: package(),
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
+    ]
+  end
+
+  defp description do
+    "A helper to build the most common database queries for Ecto (and potentially other backends)."
+  end
+
+  defp package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitLab" => "https://gitlab.com/up-learn-uk/query-elf"}
     ]
   end
 
@@ -21,6 +35,7 @@ defmodule QueryElf.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
